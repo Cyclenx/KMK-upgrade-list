316a84b40fa9fbb9a76fdcecdc6a81caac5575a7
Pijan <halimhafizhan1059@gmail.com>
2026-08-12 00:23:30 +0800

    j

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import Report from './page/Report.jsx';
 import Home from './page/Home.jsx'
 import reportWebVitals from './reportWebVitals';
-import organisasi from './page/organisasi.jsx'; 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
@@ -13,10 +12,6 @@ const router = createBrowserRouter([
     path: "/",
     element: <Home />
   },
-  {
-    path: "/organisasi",
-    element: <organisasi /> 
-   },
   {
     path: "/Cadangan",
     element: <Report />
