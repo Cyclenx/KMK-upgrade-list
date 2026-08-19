21b17f4a1f3d65bb6153739d18fc6b5095cf44e0
Pijan <halimhafizhan1059@gmail.com>
2026-08-19 17:42:14 +0800

    testt

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -141,7 +141,11 @@ export default function App() {
               </div>
             </li>
 
-            <li className="relative group cursor-pointer">
+            <li className="relative group">
+              <a
+               onClick={() => {
+                Navigation("/organisasi");
+               }
               <div className="flex items-center gap-2 px-6 py-4 text-gray-200 text-sm font-medium hover:text-white transition-colors border-b-2 border-transparent hover:border-gray-400">
                 <Users size={16} />
                 <span>Organisasi</span>
